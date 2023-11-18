fe3f4e0f335630a7df319b3f8373ae6ad08eede2
Sean Mairs <seanmairs@berkeley.edu>
2023-11-18 21:08:02 +0000

    [fix] fix for representative model method

--- app/models/representative.rb
+++ app/models/representative.rb
@@ -17,8 +17,10 @@ class Representative < ApplicationRecord
         end
       end
 
-      rep = Representative.create!({ name: official.name, ocdid: ocdid_temp,
+      # FIRST check if representative is in DB, if it is then just use that instead
+      rep = Representative.find_or_create_by({ name: official.name, ocdid: ocdid_temp,
           title: title_temp })
+
       reps.push(rep)
     end
 
