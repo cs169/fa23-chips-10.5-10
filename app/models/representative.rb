725f0531435b7a67a41065403e41907dff97008f
aherston <77765004+aherston@users.noreply.github.com>
2023-11-29 14:55:41 -0800

    Update representative.rb

--- app/models/representative.rb
+++ app/models/representative.rb
@@ -3,12 +3,21 @@
 class Representative < ApplicationRecord
   has_many :news_items, dependent: :delete_all
 
+  validates :address, :city, :state, :zip, :party, :photo_url, presence: true
+
   def self.civic_api_to_representative_params(rep_info)
     reps = []
 
     rep_info.officials.each_with_index do |official, index|
       ocdid_temp = ''
       title_temp = ''
+      
+      address_temp = official.address[0].line1
+      city_temp = official.address[0].city
+      state_temp = official.address[0].state
+      zip_temp = official.address[0].zip
+      party_temp = official.party
+      photo_url_temp = official.photoUrl
 
       rep_info.offices.each do |office|
         if office.official_indices.include? index
@@ -17,8 +26,11 @@ class Representative < ApplicationRecord
         end
       end
 
-      rep = Representative.create!({ name: official.name, ocdid: ocdid_temp,
-          title: title_temp })
+      # FIRST check if representative is in DB, if it is then just use that instead
+      rep = Representative.find_or_create_by({ name: official.name, ocdid: ocdid_temp,
+          title: title_temp, address: address_temp, city: city_temp, state: state_temp, 
+          zip: zip_temp, party: party_temp, photo_url: photo_url_temp })
+
       reps.push(rep)
     end
 
