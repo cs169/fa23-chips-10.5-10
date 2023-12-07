418485802235ff3bbb9f9a61e15ed088675d2592
Alice Herston <aherston@berkeley.edu>
2023-12-07 08:05:17 +0000

    add issue field to news items (2.1)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_12_03_221015) do
+ActiveRecord::Schema.define(version: 2023_12_07_065742) do
 
   create_table "campaign_finances", force: :cascade do |t|
     t.string "name"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2023_12_03_221015) do
     t.integer "representative_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "issue"
     t.index ["representative_id"], name: "index_news_items_on_representative_id"
   end
 
